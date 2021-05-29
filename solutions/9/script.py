938ad46546096567320f77218bac294428f5304d
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-05-29 17:49:57 +0000

    Modify Pythagorean Triplets solution

--- solutions/9/script.py
+++ solutions/9/script.py
@@ -1,23 +1,21 @@
 from mathematics.numbers import isqrt
-from itertools import combinations_with_replacement
 
 sum_to = int(input())
 
-def generate_triplets(m, n, k):
-    if (m <= n):
-        return 0, 0, 0
-    a = k * (m ** 2 - n ** 2)
-    b = k * 2 * m * n
-    c = k * (m ** 2 + n ** 2)
+def generate_triplets(m, n):
+    a = (m ** 2 - n ** 2)
+    b = 2 * m * n
+    c = (m ** 2 + n ** 2)
     return a, b, c
-
 
 def find_product():
     ceiling = isqrt(sum_to) + 1
-    for k in range(1, ceiling):
-        for n, m in combinations_with_replacement(range(1, ceiling), 2):
-            a, b, c = generate_triplets(m, n, k)
-            if a + b + c == sum_to:
-                return a * b * c
+    for m in range(1, ceiling):
+        for n in range(1, m):
+            a, b, c = generate_triplets(m, n)
+            current_sum = a + b + c
+            if current_sum != 0 and sum_to % current_sum == 0:
+                k = sum_to // current_sum
+                return a * b * c * (k ** 3)
 
 print(find_product())
