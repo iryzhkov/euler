528c88435e5b1d32af4f2035e0d5b6195ddecbfb
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-05-29 22:37:00 +0000

    templates for solutions 12,13

---
+++ solutions/12/script.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+height, width, n = map(int, input().split())
+array = [list(map(int, input().split())) for _ in range(height)]
+array = np.array(array)
+print("not solved yet")
